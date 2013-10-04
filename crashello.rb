fc3e2e8dea180789f8f4e70185a868f507f47a73
izzyjohnston <izzy@quirkyinc.com>
2013-10-04 09:28:17 -0400

    Updating params for better string

--- crashello.rb
+++ crashello.rb
@@ -13,30 +13,34 @@ configure do
   @@board = Trello::Board.all.find {|board| board.name == ENV['TRELLO_BOARD_NAME']}
 end
 
-post '/kaboom' do
+post '/kaboom/:board-name' do
+  list = nil
   if !params['board-name'].nil?
-    list = board.lists.find {|list| list.name == params['board-name']}
-    if !list.nil?
-      trello_list_id = list.id
-  
-      params = JSON.parse(request.body.read.to_s)
-
-      if params['event'] == 'issue_impact_change' && params['payload']
-        payload = params['payload']
-
-        puts "Creating card with title: #{payload['title']} with details #{payload['url']}"
-
-        Trello::Card.create(
-          :name => "CRASH: #{payload['title']}\n#{payload['method']}",
-          :description => "[Crashlytics](#{payload['url']})",
-          :list_id => trello_list_id
-          )
-      end
-    else
-      puts "Error posting to Trello: Invalid board name"
-    end
+    list = board.lists.find {|list| list.name == params[:board-name]}
+  elsif defined? ENV['TRELLO_LIST_NAME']
+    list = board.lists.find {|list| list.name == ENV['TRELLO_LIST_NAME']}
   else
     puts "Error posting to Trello: Trello board name not set"
   end
+  if !list.nil?
+    trello_list_id = list.id
+
+    params = JSON.parse(request.body.read.to_s)
+
+    if params['event'] == 'issue_impact_change' && params['payload']
+      payload = params['payload']
+
+      puts "Creating card with title: #{payload['title']} with details #{payload['url']}"
+
+      Trello::Card.create(
+        :name => "CRASH: #{payload['title']}\n#{payload['method']}",
+        :description => "[Crashlytics](#{payload['url']})",
+        :list_id => trello_list_id
+        )
+    end
+  else
+    puts "Error posting to Trello: Invalid board name"
+  end
+  
 end
 
