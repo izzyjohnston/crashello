ddcc0db781c4b514f620d8b8c071de0a54d1ea56
izzyjohnston <izzy@quirkyinc.com>
2013-10-04 09:21:27 -0400

    Updating so that board names can be set as a parameter

--- crashello.rb
+++ crashello.rb
@@ -13,42 +13,30 @@ configure do
   @@board = Trello::Board.all.find {|board| board.name == ENV['TRELLO_BOARD_NAME']}
 end
 
-post '/android-wink' do
-  list = board.lists.find {|list| list.name == ENV['ANDROID_WINK_NAME']}
-
-  trello_list_id = list.id
+post '/kaboom' do
+  if !params['board-name'].nil?
+    list = board.lists.find {|list| list.name == params['board-name']}
+    if !list.nil?
+      trello_list_id = list.id
   
-  params = JSON.parse(request.body.read.to_s)
+      params = JSON.parse(request.body.read.to_s)
 
-  if params['event'] == 'issue_impact_change' && params['payload']
-    payload = params['payload']
+      if params['event'] == 'issue_impact_change' && params['payload']
+        payload = params['payload']
 
-    puts "Creating card with title: #{payload['title']} with details #{payload['url']}"
+        puts "Creating card with title: #{payload['title']} with details #{payload['url']}"
 
-    Trello::Card.create(
-      :name => "CRASH: #{payload['title']}\n#{payload['method']}",
-      :description => "[Crashlytics](#{payload['url']})",
-      :list_id => trello_list_id
-      )
-  end
-end
-
-post '/android-porkfolio' do
-  list = board.lists.find {|list| list.name == ENV['ANDROID_PORKFOLIO_NAME']}
-
-  trello_list_id = list.id
-  params = JSON.parse(request.body.read.to_s)
-
-  if params['event'] == 'issue_impact_change' && params['payload']
-    payload = params['payload']
-
-    puts "Creating card with title: #{payload['title']} with details #{payload['url']}"
-
-    Trello::Card.create(
-      :name => "CRASH: #{payload['title']}\n#{payload['method']}",
-      :description => "[Crashlytics](#{payload['url']})",
-      :list_id => trello_list_id
-      )
+        Trello::Card.create(
+          :name => "CRASH: #{payload['title']}\n#{payload['method']}",
+          :description => "[Crashlytics](#{payload['url']})",
+          :list_id => trello_list_id
+          )
+      end
+    else
+      puts "Error posting to Trello: Invalid board name"
+    end
+  else
+    puts "Error posting to Trello: Trello board name not set"
   end
 end
 
