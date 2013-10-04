bea5b2384500ec389cb63dfcade6ca7764291df2
izzyjohnston <izzy@quirkyinc.com>
2013-10-04 09:14:07 -0400

    Updating to support multiple boards. TODO: Allow any number of boards with params

--- crashello.rb
+++ crashello.rb
@@ -10,13 +10,14 @@ Trello.configure do |config|
 end
 
 configure do
-  board = Trello::Board.all.find {|board| board.name == ENV['TRELLO_BOARD_NAME']}
-  list = board.lists.find {|list| list.name == ENV['TRELLO_LIST_NAME']}
-
-  @@trello_list_id = list.id
+  @@board = Trello::Board.all.find {|board| board.name == ENV['TRELLO_BOARD_NAME']}
 end
 
-post '/kaboom' do
+post '/android-wink' do
+  list = board.lists.find {|list| list.name == ENV['ANDROID_WINK_NAME']}
+
+  trello_list_id = list.id
+  
   params = JSON.parse(request.body.read.to_s)
 
   if params['event'] == 'issue_impact_change' && params['payload']
@@ -27,7 +28,26 @@ post '/kaboom' do
     Trello::Card.create(
       :name => "CRASH: #{payload['title']}\n#{payload['method']}",
       :description => "[Crashlytics](#{payload['url']})",
-      :list_id => @@trello_list_id
+      :list_id => trello_list_id
+      )
+  end
+end
+
+post '/android-porkfolio' do
+  list = board.lists.find {|list| list.name == ENV['ANDROID_PORKFOLIO_NAME']}
+
+  trello_list_id = list.id
+  params = JSON.parse(request.body.read.to_s)
+
+  if params['event'] == 'issue_impact_change' && params['payload']
+    payload = params['payload']
+
+    puts "Creating card with title: #{payload['title']} with details #{payload['url']}"
+
+    Trello::Card.create(
+      :name => "CRASH: #{payload['title']}\n#{payload['method']}",
+      :description => "[Crashlytics](#{payload['url']})",
+      :list_id => trello_list_id
       )
   end
 end
